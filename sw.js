a58b2f60fab86487553855ec15e89c8cdaf5a367
shirilnl <shirilelenl94@gmail.com>
2026-07-10 11:05:28 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "nls-v1";
+
+const urlsToCache = [
+
+"/",
+
+"/index.html",
+
+"/style.css",
+
+"/script.js",
+
+"/images/profile.jpg",
+
+"/vcard.vcf"
+
+];
+
+self.addEventListener("install", event=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+
+.then(cache=>{
+
+return cache.addAll(urlsToCache);
+
+})
+
+);
+
+});
+
+self.addEventListener("fetch", event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
