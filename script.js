6bc1395f75e17cfd35bde0ee3203f39d672a255f
shirilnl <shirilelenl94@gmail.com>
2026-07-10 17:08:00 +0200

    script.js

--- script.js
+++ script.js
@@ -45,6 +45,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
         });
 
+       if ("serviceWorker" in navigator) {
+
+    window.addEventListener("load", () => {
+
+        navigator.serviceWorker
+            .register("./service-worker.js")
+            .then(registration => {
+
+                console.log(
+                    "Service Worker Registered",
+                    registration.scope
+                );
+
+            })
+            .catch(error => {
+
+                console.error(
+                    "Service Worker Registration Failed",
+                    error
+                );
+
+            });
+
+    });
+
+}
+
     }
 
     console.log("Website Loaded Successfully");
